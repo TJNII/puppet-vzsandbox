1e3ad1d1bbb393d9388ba5817c5001f6e21c3f50
Tom Noonan II <tom@tjnii.com>
2013-11-16 14:42:18 -0600

    Added license blurb

--- files/shell/usr/local/bin/vzsandbox-shell.py
+++ files/shell/usr/local/bin/vzsandbox-shell.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python -tt
+# Copyright 2013 Tom Noonan II (TJNII)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Load balancer API script
+#
+# This is the shell script for the sandbox environment, intended to be
+# called as a login shell
+#
+# This script obtains a VM via the API and opens a SSH session to it.
+#
 
 import sys
 import os
